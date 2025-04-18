c6f404b40cff5a9a5cc7d950a275e761b23c7cec
FullIdle <2675423979@qq.com>
2025-04-18 12:12:45 +0800

    ...

--- src/main/kotlin/org/figsq/cobblemonplugin/cobblemonplugin/Main.kt
+++ src/main/kotlin/org/figsq/cobblemonplugin/cobblemonplugin/Main.kt
@@ -1,25 +1,22 @@
 package org.figsq.cobblemonplugin.cobblemonplugin
 
-import com.cobblemon.mod.common.api.events.CobblemonEvents
-import com.cobblemon.mod.common.battles.BattleRegistry
+import com.cobblemon.mod.common.api.pokemon.PokemonSpecies
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
-import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin(){
     override fun onEnable() {
-
+        logger.info("§c=======================")
+        val species = PokemonSpecies.random();
+        logger.info(species.translatedName.string)
+        logger.info("英语重找结果>>${PokemonSpecies.species.find { it.name == species.name }}")
+        logger.info("中文重找结果>>${PokemonSpecies.species.find { it.translatedName.string == species.translatedName.string }}")
+        logger.info("§c=======================")
     }
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
-        BattleRegistry.getBattleByParticipatingPlayerId((sender as Player).uniqueId)?.let {
-            if (args!!.isEmpty()) {
-                it.stop()
-                return false
-            }
-            it.end()
-        }
+
         return false
     }
 }
